0ea23cdfdbd598044381963015b178ec9ecefc7b
Volodymyr Kohut <vkohut@llnw.com>
2017-08-22 10:44:20 +0300

    a bit more of heroku stuff

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
-read -s -p "Enter google Password: " mypassword
-while :
-do
+if [ -z "$1" ]
+  then
+    read -s -p "Enter google Password: " mypassword
+  else
+    mypassword=$1
+fi
+#while :
+#do
     echo 'PID:'  $$
     python titlereader.py -u vvkohut@gmail.com -p $mypassword -pl 'Radio Roks Ballads' -s http://online-radioroks2.tavrmedia.ua/RadioROKS_Ballads
-done
+#done
